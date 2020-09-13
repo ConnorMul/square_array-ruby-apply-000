8845744827290cd6254304381b080a3c31388eac
Connor Mulholland <mulhollandconnor@icloud.com>
2020-09-13 22:08:45 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   new_array = []
   array.each do |exponent|
-   exponent_sq = |exponent| ** |exponent|
+   exponent_sq = exponent ** exponent
    new_array << exponent_sq
  end
 end
